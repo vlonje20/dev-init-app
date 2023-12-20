8288150b4e2ecfe19a488c12809034e3a9066678
vlonje20 <vlonje20@gmail.com>
2023-12-20 21:11:46 +0800

    adding installation files to the prep101 branch

---
+++ git-github-workflow.rb
@@ -0,0 +1,13 @@
+# Git and GitHub:
+
+working area/environmet: 
+ - git add . = add all the files in the present working directory 
+ - git add fileName = add the selected file 
+ - git add file1 file4 = add selected files 
+staging area: 
+ - git commit -m "commit message"  
+production area 
+ - git push github-URL branchName 
+   git push git remote add origin https://github.com/vlonje20/jenkins-CICD.git main 
+ - git remote add aliasName url 
+ - git push aliasName branchName
